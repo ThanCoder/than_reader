b4cc0faa13af786663f29c1a4b2685eae2d37db2
thancoder <thancoder2019@gmail.com>
2026-07-14 16:12:20 +0630

    update

--- lib/main_app/home/home_page.dart
+++ lib/main_app/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:dart_core_extensions/dart_core_extensions.dart';
 import 'package:flutter/material.dart';
-import 'package:t_widgets/t_widgets.dart';
+import 'package:t_widgets/t_widgets.dart' hide SortButton;
 import 'package:than_pkg/than_pkg.dart' hide TPlatform;
 import 'package:than_reader/core/extensions/context_extensions.dart';
 import 'package:than_reader/core/models/pdf_file.dart';
